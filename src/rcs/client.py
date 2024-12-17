50011d0daa34daeb1bfd79f0ca6857d9cb3903d3
Ivan Zhang <ivanzhangofficial@gmail.com>
2024-12-16 18:50:14 -0800

    update docs

--- src/rcs/client.py
+++ src/rcs/client.py
@@ -9,6 +9,39 @@ class Pinnacle(PinnacleBase):
     def parse_inbound_message(
         data: dict,
     ):
+        """
+        Parse the inbound message data.
+
+        Parameters
+        ----------
+        data : dict
+            The inbound message data to be parsed.
+
+        Returns
+        -------
+        inbound_message : Union[
+            InboundActionMessage,
+            InboundTextMessage,
+            InboundLocationMessage,
+            InboundMediaMessage,
+        ]
+            The parsed inbound message data.
+
+
+        Examples
+        --------
+        from rcs import Pinnacle
+
+        data = {
+            "message_type": "text",
+            "text": "Hello, World!",
+            "from": "+12345678901",
+            "to": "+10987654321",
+            "metadata": None
+        }
+        parsed_data = Pinnacle.parse_inbound_message(data)
+        """
+
         return parse_inbound(data)
 
 
@@ -18,4 +51,36 @@ class AsyncPinnacle(AsyncPinnacleBase):
     def parse_inbound_message(
         data: dict,
     ):
+        """
+        Parse the inbound message data.
+
+        Parameters
+        ----------
+        data : dict
+            The inbound message data to be parsed.
+
+        Returns
+        -------
+        inbound_message : Union[
+            InboundActionMessage,
+            InboundTextMessage,
+            InboundLocationMessage,
+            InboundMediaMessage,
+        ]
+            The parsed inbound message data.
+
+
+        Examples
+        --------
+        from rcs import Pinnacle
+
+        data = {
+            "message_type": "text",
+            "text": "Hello, World!",
+            "from": "+12345678901",
+            "to": "+10987654321",
+            "metadata": None
+        }
+        parsed_data = Pinnacle.parse_inbound_message(data)
+        """
         return parse_inbound(data)
